4e24bb9b87ff7cf7e2dfa4b6a2e8b2c8e80fdde9
Raulscj <92169825+Raulscj@users.noreply.github.com>
2023-02-04 11:12:12 -0400

    Cambios basic

--- crud.js
+++ crud.js
@@ -1,68 +1,68 @@
 //Creacion de Objeto del Inventario
 
 class Inventario {
-    constructor(nombre, cantidad, peso, precio, unidad, id) {
-        this.nombre = nombre;
-        this.cantidad = cantidad;
-        this.peso = peso;
-        this.precio = precio;
-        this.unidad = unidad;
-        this.id = id;
+  constructor(nombre, cantidad, peso, precio, unidad, id) {
+    this.nombre = nombre;
+    this.cantidad = cantidad;
+    this.peso = peso;
+    this.precio = precio;
+    this.unidad = unidad;
+    this.id = id;
+  }
+
+  //Crear y Guardar
+
+  static CreateObject() {
+    let object = new Inventario(
+      document.getElementById("nombreProducto").value,
+      document.getElementById("cantidadProducto").value,
+      document.getElementById("peso").value,
+      document.getElementById("precio").value,
+      document.querySelector('input[name="unidad"]:checked').value,
+      uuid.v1()
+    );
+
+    if (localStorage.getItem("Data") === null) {
+      let inventario = [];
+      inventario.push(object);
+      localStorage.setItem("Data", JSON.stringify(inventario));
+    } else {
+      let inventario = JSON.parse(localStorage.getItem("Data"));
+      inventario.push(object);
+      localStorage.setItem("Data", JSON.stringify(inventario));
     }
+    Inventario.read();
+  }
 
-    //Crear y Guardar
-
-    static CreateObject() {
-        let object = new Inventario(
-            document.getElementById('nombreProducto').value,
-            document.getElementById('cantidadProducto').value,
-            document.getElementById('peso').value,
-            document.getElementById('precio').value,
-            document.querySelector('input[name="unidad"]:checked').value,
-            uuid.v1()
-        );
-
-        if (localStorage.getItem('Data') === null) {
-            let inventario = [];
-            inventario.push(object);
-            localStorage.setItem('Data', JSON.stringify(inventario));
+  static Duplicated(object) {
+    if (localStorage.getItem("Data") === null) {
+      return false;
+    } else {
+      let inventario = JSON.parse(localStorage.getItem("Data"));
+      inventario.forEach((i) => {
+        if (i.nombre === object.nombre && i.peso === object.peso) {
+          return true;
         } else {
-            let inventario = JSON.parse(localStorage.getItem('Data'));
-            inventario.push(object);
-            localStorage.setItem('Data', JSON.stringify(inventario));
+          return false;
         }
-        Inventario.read();
+      });
     }
+  }
 
-    static Duplicated(object) {
-        if (localStorage.getItem('Data') === null) {
-            return false;
-        } else {
-            let inventario = JSON.parse(localStorage.getItem('Data'));
-            inventario.forEach((i) => {
-                if (i.nombre === object.nombre && i.peso === object.peso) {
-                    return true;
-                } else {
-                    return false;
-                }
-            });
-        }
-    }
+  //Leer
 
-    //Leer
+  static read() {
+    let Inventario = JSON.parse(localStorage.getItem("Data"));
+    document.getElementById("t-render").innerHTML = "";
+    for (let i = 0; i < Inventario.length; i++) {
+      let nombre = Inventario[i].nombre;
+      let cantidad = Inventario[i].cantidad;
+      let peso = Inventario[i].peso;
+      let precio = Inventario[i].precio;
+      let unidad = Inventario[i].unidad;
+      let id = Inventario[i].id;
 
-    static read() {
-        let Inventario = JSON.parse(localStorage.getItem('Data'));
-        document.getElementById('t-render').innerHTML = '';
-        for (let i = 0; i < Inventario.length; i++) {
-            let nombre = Inventario[i].nombre;
-            let cantidad = Inventario[i].cantidad;
-            let peso = Inventario[i].peso;
-            let precio = Inventario[i].precio;
-            let unidad = Inventario[i].unidad;
-            let id = Inventario[i].id;
-
-            document.getElementById('t-render').innerHTML += `
+      document.getElementById("t-render").innerHTML += `
         <tr>
             <td scope="row">1</th>
             <td class="text-end">${nombre}</td>
@@ -70,12 +70,14 @@ class Inventario {
             <td class="text-end">${peso}</td>
             <td class="text-end">${precio}</td>
             <td class="text-end">${unidad}</td>
-            <td class="d-flex justify-content-center">
+            <td class="text-end">
                 <div class="btn-group" role="group" aria-label="Basic example">
+                <!-- Editar -->
                 <button type="button" class="btn btn-tertiary p-2">
                     <i class="fa-solid fa-pen"></i>
                 </button>
-                <button type="button" class="btn btn-tertiary color-danger p-2">
+                <!-- Eliminar -->
+                <button type="button" class="btn btn-tertiary color-danger p-2" onclick="Inventario.Eliminar(${nombre})">
                     <i class="fa-solid fa-trash"></i>
                 </button>
                 </div>
@@ -83,70 +85,66 @@ class Inventario {
             <td class="text-end">${id}</td>
         </tr>
         `;
-        }
     }
+  }
 
-    //editar
+  //editar
 
-    static edit(name) {
-        let Inventario = localStorage.getItem(JSON.parse('Inventario'));
-        Inventario.forEach((i) => {
-            if (i.nombre == name) {
-                document.getElementById(
-                    'ID DEL INPUT A LLENAR CON DATA'
-                ).value = i.nombre;
-                document.getElementById(
-                    'ID DEL INPUT A LLENAR CON DATA'
-                ).value = i.marca;
-                document.getElementById(
-                    'ID DEL INPUT A LLENAR CON DATA'
-                ).value = i.descripcion;
-                document.getElementById(
-                    'ID DEL INPUT A LLENAR CON DATA'
-                ).value = i.precio;
-                document.getElementById(
-                    'ID DEL INPUT NO DISPLAY DE LA POSICION'
-                ).value = i;
-            }
-        });
-    }
+  static edit(name) {
+    let Inventario = localStorage.getItem(JSON.parse("Inventario"));
+    Inventario.forEach((i) => {
+      if (i.nombre == name) {
+        document.getElementById("ID DEL INPUT A LLENAR CON DATA").value =
+          i.nombre;
+        document.getElementById("ID DEL INPUT A LLENAR CON DATA").value =
+          i.marca;
+        document.getElementById("ID DEL INPUT A LLENAR CON DATA").value =
+          i.descripcion;
+        document.getElementById("ID DEL INPUT A LLENAR CON DATA").value =
+          i.precio;
+        document.getElementById(
+          "ID DEL INPUT NO DISPLAY DE LA POSICION"
+        ).value = i;
+      }
+    });
+  }
 
-    //Actualizar
+  //Actualizar
 
-    static Actualizar(i) {
-        let Inventario = localStorage.getItem(JSON.parse('Inventario'));
-        Inventario[i].nombre = document.getElementById('ID DEL INPUT DEL FORM');
-        Inventario[i].marca = document.getElementById('ID DEL INPUT DEL FORM');
-        Inventario[i].descripcion = document.getElementById(
-            'ID DEL INPUT DEL FORM'
-        );
-        Inventario[i].precio = document.getElementById('ID DEL INPUT DEL FORM');
-        localStorage.setItem('Inventario', JSON.stringify(Inventario));
-    }
+  static Actualizar(i) {
+    let Inventario = localStorage.getItem(JSON.parse("Inventario"));
+    Inventario[i].nombre = document.getElementById("ID DEL INPUT DEL FORM");
+    Inventario[i].marca = document.getElementById("ID DEL INPUT DEL FORM");
+    Inventario[i].descripcion = document.getElementById(
+      "ID DEL INPUT DEL FORM"
+    );
+    Inventario[i].precio = document.getElementById("ID DEL INPUT DEL FORM");
+    localStorage.setItem("Inventario", JSON.stringify(Inventario));
+  }
 
-    //Eliminar
+  //Eliminar
 
-    static Eliminar(name) {
-        let Inventario = localStorage.getItem(JSON.parse('Inventario'));
-        Inventario.forEach((i) => {
-            if (i.nombre === tittle) {
-                Inventario.slice(i, 1);
-            }
-        });
-        localStorage.setItem('Inventario', JSON.stringify(Inventario));
-    }
+  static Eliminar(name) {
+    let Inventario = localStorage.getItem(JSON.parse("Inventario"));
+    Inventario.forEach((i) => {
+      if (i.nombre === name) {
+        Inventario.slice(i, 1);
+      }
+    });
+    localStorage.setItem("Inventario", JSON.stringify(Inventario));
+  }
 
-    //Exportar
+  //Exportar
 
-    static Export() {
-        let object = localStorage.getItem('Data');
-        let data = 'data:text/json;charset=utf-8,' + encodeURIComponent(object);
-        let download = document.createElement('a');
-        download.setAttribute('href', data);
-        download.setAttribute('download', 'Exportacion' + '.json');
-        download.click();
-        download.remove();
-    }
+  static Export() {
+    let object = localStorage.getItem("Data");
+    let data = "data:text/json;charset=utf-8," + encodeURIComponent(object);
+    let download = document.createElement("a");
+    download.setAttribute("href", data);
+    download.setAttribute("download", "Exportacion" + ".json");
+    download.click();
+    download.remove();
+  }
 }
 
 Inventario.read();
